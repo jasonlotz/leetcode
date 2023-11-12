4d34e31258ea694d3da97e82ba7445004e478e58
Jason Lotz <jason.lotz@merative.com>
2023-11-12 12:25:56 -0500

    Completed #217 and #242.

---
+++ 217.contains-duplicate.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=217 lang=python3
+#
+# [217] Contains Duplicate
+#
+
+# @lc code=start
+
+# Solution #1: Using a set
+# Time complexity: O(n)
+# Space complexity: O(n)
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for i in nums:
+            if i in hashset:
+                return True
+            hashset.add(i)
+
+# Solution #1: Sorting first
+# Time complexity: O(n log(n))
+# Space complexity: O(1)
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         nums.sort()
+
+#         for i in range(len(nums) - 1):
+#             if nums[i] == nums[i + 1]:
+#                 return True
+
+#         return False
+
+# @lc code=end
---
+++ 242.valid-anagram.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=242 lang=python3
+#
+# [242] Valid Anagram
+#
+
+# @lc code=start
+#
+
+# Solution #1: Using dicts
+# Time complexity: O(n)
+# Space complexity: O(n)
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        s_dict = {}
+        t_dict = {}
+        for i in s:
+            if i in range(len(s_dict)):
+                s_dict[i] = 1 + s_dict.get(i, 0)
+                t_dict[i] = 1 + t_dict.get(i, 0)
+
+        return s_dict == t_dict
+
+# Solution #2: Sorting first
+# Time complexity: O(n log(n))
+# Space complexity: O(1)
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         return sorted(s) == sorted(t)
+
+# @lc code=end
