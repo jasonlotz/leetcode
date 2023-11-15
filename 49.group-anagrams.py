78410504976747d3302c62f8a71369cab70ee620
jasonlotz <jasonlotz@gmail.com>
2023-11-15 09:16:35 -0500

    Completed #49.

---
+++ 49.group-anagrams.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=49 lang=python3
+#
+# [49] Group Anagrams
+#
+
+# @lc code=start
+
+# Solution #1: Using an array of letters as keys in a hashmap of anagrams
+# Time complexity: O(m * n) [m = average string length, n = number of strings]
+# Space complexity: O(n)
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        result = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+
+            result[tuple(count)].append(s)
+        
+        return result.values()
+        
+# @lc code=end
+
