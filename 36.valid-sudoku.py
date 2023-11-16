df606ecfda7693f3fb00bf18325f227763a3442f
jasonlotz <jasonlotz@gmail.com>
2023-11-16 16:05:32 -0500

    Completed #36.

---
+++ 36.valid-sudoku.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=36 lang=python3
+#
+# [36] Valid Sudoku
+#
+
+# @lc code=start
+
+from collections import defaultdict
+
+
+# Solution: Use 3 hashsets to store the values of each row, column and 9x9 squares
+# Time complexity: O(1)
+# Space complexity: O(1)
+class Solution:
+    def isValidSudoku(self, board: list[list[str]]) -> bool:
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)  # key: (row / 3, col / 3)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+
+                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r // 3, c // 3)]:
+                    return False
+
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+
+        return True
+
+# @lc code=end
