fca71d15f4f9bc9c8510069ec8239013aaf09331
jasonlotz <jasonlotz@gmail.com>
2023-11-25 10:38:55 -0500

    Completed # 14.

---
+++ 14.longest-common-prefix.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=14 lang=python3
+#
+# [14] Longest Common Prefix
+#
+
+# @lc code=start
+
+# Time complexity: O(n)
+# Space complexity: O(1)
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        prefix = None
+
+        for str in strs:
+            if prefix is None:
+                prefix = str
+
+            while not str.startswith(prefix):
+                prefix = prefix[:-1]
+
+        return prefix if prefix is not None else ""
+
+# @lc code=end
