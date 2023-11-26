755cb146ffe29485bd3ab411f716d3396ce089f7
jasonlotz <jasonlotz@gmail.com>
2023-11-26 11:22:35 -0500

    Added space complexity.

--- 21.merge-two-sorted-lists.py
+++ 21.merge-two-sorted-lists.py
@@ -18,6 +18,7 @@ class ListNode:
 
 # Solution: Recursion
 # Time Complexity: O(n + m)
+# Space Complexity: O(n + m)
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         if list1 is None:
