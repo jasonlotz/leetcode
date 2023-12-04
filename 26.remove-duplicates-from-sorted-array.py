f32ae60fc7d2f915c231e5c9377a09794ca98e29
jasonlotz <jasonlotz@gmail.com>
2023-12-04 17:40:25 -0500

    Completed #26.

---
+++ 26.remove-duplicates-from-sorted-array.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=26 lang=python3
+#
+# [26] Remove Duplicates from Sorted Array
+#
+
+# @lc code=start
+
+# Solution: Two Pointers
+# Time complexity: O(n)
+# Space complexity: O(1)
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+        if len(nums) == 0:
+            return 0
+
+        left = 0
+        for right in range(1, len(nums)):
+            if nums[right] != nums[left]:
+                left += 1
+                nums[left] = nums[right]
+
+        return left+1
+
+# @lc code=end
