a65b2f09a3e974a76c5e8ec2caa09edfaded8d84
jasonlotz <jasonlotz@gmail.com>
2023-12-04 21:11:26 -0500

    Completed 1929.

---
+++ 1929.concatenation-of-array.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=1929 lang=python3
+#
+# [1929] Concatenation of Array
+#
+
+# @lc code=start
+
+# Solution: Loop and append
+# Time complexity: O(n)
+# Space complexity: O(1)
+class Solution:
+    def getConcatenation(self, nums: list[int]) -> list[int]:
+        for i in range(len(nums)):
+            nums.append(nums[i])
+
+        return nums
+# @lc code=end
