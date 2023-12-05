86de40cb3a5a23b0509feef5f827c51f24f38cc7
jasonlotz <jasonlotz@gmail.com>
2023-12-04 21:15:09 -0500

    Completed 682.

---
+++ 682.baseball-game.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=682 lang=python3
+#
+# [682] Baseball Game
+#
+
+# @lc code=start
+
+# Solution: Stack
+# Time complexity: O(n)
+# Space complexity: O(n)
+class Solution:
+    def calPoints(self, operations: list[str]) -> int:
+        score_stack = []
+
+        for op in operations:
+            if op == '+':
+                score_stack.append(score_stack[-1] + score_stack[-2])
+            elif op == 'D':
+                score_stack.append(score_stack[-1] * 2)
+            elif op == 'C':
+                score_stack.pop()
+            else:
+                score_stack.append(int(op))
+
+        return sum(score_stack)
+# @lc code=end
