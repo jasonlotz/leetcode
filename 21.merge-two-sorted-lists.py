faf1b52dc2e81f66739aa287acf2b97acc0c6a5c
jasonlotz <jasonlotz@gmail.com>
2023-12-06 11:22:30 -0500

    Added iteration solution for #21.

--- 21.merge-two-sorted-lists.py
+++ 21.merge-two-sorted-lists.py
@@ -19,18 +19,40 @@ class ListNode:
 # Solution: Recursion
 # Time Complexity: O(n + m)
 # Space Complexity: O(1)
+# class Solution:
+#     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+#         if list1 is None:
+#             return list2
+#         elif list2 is None:
+#             return list1
+#         elif list1.val < list2.val:
+#             list1.next = self.mergeTwoLists(list1.next, list2)
+#             return list1
+#         else:
+#             list2.next = self.mergeTwoLists(list1, list2.next)
+#             return list2
+
+
+# Solution: Iteration
+# Time Complexity: O(n + m)
+# Space Complexity: O(1)
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if list1 is None:
-            return list2
-        elif list2 is None:
-            return list1
-        elif list1.val < list2.val:
-            list1.next = self.mergeTwoLists(list1.next, list2)
-            return list1
-        else:
-            list2.next = self.mergeTwoLists(list1, list2.next)
-            return list2
+        dummy = ListNode()
+        tail = dummy
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        if list1:
+            tail.next = list1
+        elif list2:
+            tail.next = list2
+        return dummy.next
 
 
 # @lc code=end
