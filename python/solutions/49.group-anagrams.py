e99fe657e84b1b39792a5216c0b3d78c4390b1b3
jasonlotz <jasonlotz@gmail.com>
2023-12-20 15:26:09 -0500

    Updated solution description for #49.

--- python/solutions/49.group-anagrams.py
+++ python/solutions/49.group-anagrams.py
@@ -6,24 +6,31 @@
 
 # @lc code=start
 
-# Solution #1: Using an array of letters as keys in a hashmap of anagrams
+# Solution #1:
+#   For each word, create a tuple of 26 elements where each element is the count of the character in the word.
+#   Store the tuple as key in a dictionary and append the word to the list of values for the key.
+#   Return the values of the dictionary.
 # Time complexity: O(m * n) [m = average string length, n = number of strings]
 # Space complexity: O(n)
 from collections import defaultdict
 
 
 class Solution:
-    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        result = defaultdict(list)
+  def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+    result = defaultdict(list)
 
-        for s in strs:
-            count = [0] * 26
+    for s in strs:
+      count = [0] * 26
 
-            for c in s:
-                count[ord(c) - ord('a')] += 1
+      for c in s:
+        count[ord(c) - ord('a')] += 1
 
-            result[tuple(count)].append(s)
+      result[tuple(count)].append(s)
+    print(result)
+    return result.values()
 
-        return result.values()
+
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+print(Solution().groupAnagrams(strs))
 
 # @lc code=end
