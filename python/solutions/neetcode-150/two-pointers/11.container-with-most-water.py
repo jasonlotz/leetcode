cdcafb051cdf319d4c8cdeb320418a7fc1bbb903
jasonlotz <jasonlotz@gmail.com>
2023-12-25 11:15:30 -0500

    Completed #11.

---
+++ python/solutions/neetcode-150/two-pointers/11.container-with-most-water.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=11 lang=python3
+#
+# [11] Container With Most Water
+#
+
+# @lc code=start
+class Solution:
+  def maxArea(self, height: list[int]) -> int:
+    max_area = 0
+
+    left_index = 0
+    right_index = len(height) - 1
+
+    while left_index < right_index:
+      area = (min(height[left_index], height[right_index])
+              * (right_index - left_index))
+
+      if height[left_index] < height[right_index]:
+        left_index += 1
+      else:
+        right_index -= 1
+
+      max_area = max(max_area, area)
+    return max_area
+
+
+assert Solution().maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]) == 49
+assert Solution().maxArea([1, 1]) == 1
+# @lc code=end
