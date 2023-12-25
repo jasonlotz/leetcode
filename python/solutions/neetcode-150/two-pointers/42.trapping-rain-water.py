bd2d690d8e76f80f21e9ffdd13b86bf8d62e45a7
jasonlotz <jasonlotz@gmail.com>
2023-12-25 15:22:36 -0500

    Completed #42.

---
+++ python/solutions/neetcode-150/two-pointers/42.trapping-rain-water.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=42 lang=python3
+#
+# [42] Trapping Rain Water
+#
+
+# @lc code=start
+
+# Solution 1: Two pointers
+# Time complexity: O(n)
+# Space complexity: O(1)
+class Solution:
+  def trap(self, height: list[int]) -> int:
+    if not height:
+      return 0
+
+    left, right = 0, len(height) - 1
+    left_max, right_max = height[left], height[right]
+    result = 0
+
+    while left < right:
+      if left_max < right_max:
+        left += 1
+        left_max = max(left_max, height[left])
+        result += left_max - height[left]
+      else:
+        right -= 1
+        right_max = max(right_max, height[right])
+        result += right_max - height[right]
+
+    return result
+# @lc code=end
