931510d1f8ec5ca5fe11cce47b6fe8b0d54c1ce2
jasonlotz <jasonlotz@gmail.com>
2023-12-31 16:03:24 -0500

    Completed #704.

---
+++ python/solutions/neetcode-150/binary-search/704.binary-search.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=704 lang=python3
+#
+# [704] Binary Search
+#
+
+# @lc code=start
+class Solution:
+  def search(self, nums: list[int], target: int) -> int:
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+      mid = (right + left) // 2
+      if nums[mid] == target:
+        return mid
+      elif nums[mid] > target:
+        right = mid - 1
+      elif nums[mid] < target:
+        left = mid + 1
+
+    return -1
+
+
+assert Solution().search([-1, 0, 3, 5, 9, 12], 9) == 4
+# @lc code=end
