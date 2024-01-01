cede9b0d2886dd875073cb9af54f4e2421f8b502
jasonlotz <jasonlotz@gmail.com>
2024-01-01 13:05:45 -0500

    Completed #74.

---
+++ python/solutions/neetcode-150/binary-search/74.search-a-2-d-matrix.py
@@ -0,0 +1,48 @@
+#
+# @lc app=leetcode id=74 lang=python3
+#
+# [74] Search a 2D Matrix
+#
+
+# @lc code=start
+# Solution: Binary search to find row and then binary search on row to find value
+# Time complexity: O(log(m * n))
+# Space complexity: O(1)
+class Solution:
+  def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
+    ROWS, COLS = len(matrix), len(matrix[0])
+
+    top, bottom = 0, ROWS - 1
+
+    while top <= bottom:
+      row = (top + bottom) // 2
+      if target > matrix[row][-1]:
+        top = row + 1
+      elif target < matrix[row][0]:
+        bottom = row - 1
+      else:
+        break
+
+    if not (top <= bottom):
+      return False
+
+    row = (top + bottom) // 2
+    left, right = 0, COLS - 1
+
+    while left <= right:
+      mid = (left + right) // 2
+      if target > matrix[row][mid]:
+        left = mid + 1
+      elif target < matrix[row][mid]:
+        right = mid - 1
+      else:
+        return True
+
+    return False
+
+
+assert Solution().searchMatrix(
+    [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3) is True
+assert Solution().searchMatrix(
+    [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 13) is False
+# @lc code=end
