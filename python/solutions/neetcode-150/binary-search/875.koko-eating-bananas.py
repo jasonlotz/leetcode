c6387c9eaca8c4358439c380095eacfb30badf35
jasonlotz <jasonlotz@gmail.com>
2024-01-02 10:10:46 -0500

    Completed #875.

---
+++ python/solutions/neetcode-150/binary-search/875.koko-eating-bananas.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=875 lang=python3
+#
+# [875] Koko Eating Bananas
+#
+
+# @lc code=start
+# Solution: Binary search
+# Time complexity: O(NlogW), where N is the number of piles, and W is the maximum size of a pile.
+# Space complexity: O(1)
+import math
+
+
+class Solution:
+  def minEatingSpeed(self, piles: list[int], h: int) -> int:
+    left, right = 1, max(piles)
+    result = right
+
+    while left <= right:
+      mid = (left + right) // 2
+      hours = 0
+
+      for pile in piles:
+        hours += math.ceil(pile / mid)
+
+      if hours <= h:
+        result = min(result, mid)
+        right = mid - 1
+      else:
+        left = mid + 1
+
+    return result
+
+
+# @lc code=end
